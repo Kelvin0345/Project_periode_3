3cef6bedeed48701e579537b2423ef40816ab91b
kelvinilliang <kelvinfilliang04@gmail.com>
2026-03-02 14:49:44 +0100

    Eerste commit

--- Reserverings_overzicht/Index.php
+++ Reserverings_overzicht/Index.php
@@ -49,10 +49,62 @@ $result = $statement->fetchAll(PDO::FETCH_OBJ);
 </head>
 <body>
 
+<div class="container-mt-3">
+        <div class="row-justify-content-center">
+            <div class="col-8">
+                <h3>Reservering overzicht</h3>
+            </div>
+        </div>
 
+        <div class="row justidy-content-center my-3">
+            <div class="col-10"><h6>Nieuwe achtbaan <a href="./create.php"><i class="bi bi-plus-square text-danger text-danger"></i></h6></a></div>
+        </div>
 
+        <div class="row-justify-content-center mt-3">
+            <div class="col-10">
+                <table class="table table-striped table-hover ">
+                    <thead>
+                        <th>Voornaam</th>
+                        <th>Tussenvoegsel</th>
+                        <th>Achternaam</th>
+                        <th>Nummer</th>
+                        <th>Datum</th>
+                        <th</th>
+                        <th>Wijzig</th>
+                        <th>Verwijder</th>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($result as $Rollercoaster):?>
+                            <tr>
+                                <td><?= $Rollercoaster->RollerCoaster; ?></td>
+                                <td><?= $Rollercoaster->AmusementPark; ?></td>
+                                <td><?= $Rollercoaster->Country; ?></td>
+                                <td class="text-center"><?= $Rollercoaster->TopSpeed; ?></td>
+                                <td class="text-center"><?= $Rollercoaster->Height; ?></td>
+                                <td ><?= $Rollercoaster->YOFC; ?></td>
+                                
+                                <td class="text-center">
+                                    <a href="update.php?id=<?=  $Rollercoaster->Id; ?>">
+                                      <i class="bi bi-pencil-square text-success"></i>
+                                    </a>
+                                </td>
+                                
+                                <td class='text-center'>
+                                    <a href="delete.php?id=<?= $Rollercoaster->Id; ?>">
+                                        <i class="bi bi-x-square text-danger"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>   
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
-    
+    </div>
+
+</div>
+
 
 
 
