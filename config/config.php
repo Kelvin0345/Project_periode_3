ffc8dc94e60fe9094907460cf40bd61aec49e7ed
Ali102105 <alizuhy@outlook.com>
2026-03-07 17:42:09 +0100

    READ af - Medewerkers overzicht

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * De inloggegevens van de gebruiker van de database staan hieronder
+ */
+
+//De naam van onze mysql-server:
+$dbHost = 'localhost';
+
+//De naam van de database:
+$dbName = 'Project3';
+
+//Naam van de gebruiker die de queries gaat uitvoeren
+$dbUser = 'root';
+
+//Wachtwoord van de root:
+$dbPass = '';
+?>
---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+include('config/config.php');
+$dsn = "mysql:host=$dbHost;
+        dbname=$dbName;
+        charset=UTF8";
+
+$pdo = new PDO($dsn, $dbUser, $dbPass);
+
+$sql = 'SELECT MDW.Id
+               ,MDW.Voornaam
+               ,MDW.Tussenvoegsel
+               ,MDW.Achternaam
+               ,MDW.Nummer
+               ,MDW.Medewerkersoort
+        FROM    Medewerkers as MDW
+        ORDER BY MDW.Id DESC
+               ,MDW.Voornaam DESC
+               ,MDW.Tussenvoegsel DESC
+               ,MDW.Achternaam DESC
+               ,MDW.Nummer DESC
+               ,MDW.Medewerkersoort DESC';
+
+               $statement = $pdo->prepare($sql);
+ 
+               $statement->execute();
+
+               $result = $statement->fetchAll(PDO::FETCH_OBJ);
+
+               //var_dump($result);
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Project 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="cointainer mt-3">
+      <div class="row justify-content-center">
+        <div class="col-8">
+          <h3>Medewerkers Overzicht</h3>
+        </div>
+      </div>
+    </div>
+  
+    <div class="row justify-content-center">
+      <div class="col-8">
+        <!--hier komt de tabel -->
+        <table class="table table-striped table-hover">
+          <thead>
+            <th>Id</th>
+            <th>Voornaam</th>
+            <th>Tussenvoegsel</th>
+            <th>Achternaam</th>
+            <th>Nummer</th>
+            <th>Medewerkersoort</th>
+          </thead>
+          <tbody>
+            <?php foreach ($result as $medewerker):?>
+            <tr>
+              <td><?= $medewerker->Id; ?></td>
+              <td><?= $medewerker->Voornaam; ?></td>
+              <td><?= $medewerker->Tussenvoegsel; ?></td>
+              <td><?= $medewerker->Achternaam; ?></td>
+              <td><?= $medewerker->Nummer; ?></td>
+              <td><?= $medewerker->Medewerkersoort; ?></td>
+            </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
