c6030f820a89bbd58e1d1388f3b384d9dd558399
kelvinilliang <kelvinfilliang04@gmail.com>
2026-03-17 10:35:51 +0100

    query uitvoeren

--- Reserverings_overzicht/create.php
+++ Reserverings_overzicht/create.php
@@ -51,7 +51,7 @@ if (isset($_POST['submit'])) {
     $statement->bindValue(':Datum', $_POST['Datum'], PDO::PARAM_STR);
     $statement->bindValue(':Tijd', $_POST['Tijd'], PDO::PARAM_STR);
     $statement->bindValue(':Reserveringstatus', $_POST['Reserveringstatus'], PDO::PARAM_STR);
-
+    
     // query uitvoeren
     $statement->execute();
 
